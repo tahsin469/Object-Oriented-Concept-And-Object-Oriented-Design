1674bca82fe4f213d8345e4238ff920916fe5032
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-02-08 11:48:33 +0600

    Update Employee.java

--- Object-Oriented-Design/constructor/Constructor/src/constructor/Employee.java
+++ Object-Oriented-Design/constructor/Constructor/src/constructor/Employee.java
@@ -7,7 +7,7 @@ public class Employee {
     int working_hour;
     String shift;
 
-    public Employee(int emp_id, int working_hour, String shift) {
+   Employee(int emp_id, int working_hour, String shift) {
         this.emp_id = emp_id;
         this.working_hour = working_hour;
         this.shift = shift;
