da9f6e0ddfadea72d0abfca3f36c90fc3f239a7e
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-03-14 15:36:41 +0600

    Update While_Loop.java

--- java concept/LOOP/src/loop/While_Loop.java
+++ java concept/LOOP/src/loop/While_Loop.java
@@ -14,7 +14,7 @@ public class While_Loop {
     public static void main(String[] args) {
         int i = 100;
         while (i > 20) {
-            System.out.println(i);
+            System.out.println(i)
             i--;
         }
     }
