26766f47cf3d5946ffb9cd343313a3e0446d5300
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-04-06 04:33:43 +0600

    Add files via upload

---
+++ Object-Oriented-Design/object pass/objectpass/src/objectpass/a.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectpass;
+
+/**
+ *
+ * @author User
+ */
+public class a {
+    int b;
+    int c;
+
+    public a(int b, int c) {
+        this.b = b;
+        this.c = c;
+    }
+    void compair(b b1){
+        //int f = b1.f;
+        System.out.println(b1.f);
+    }
+}
---
+++ Object-Oriented-Design/object pass/objectpass/src/objectpass/b.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package objectpass;
+
+/**
+ *
+ * @author User
+ */
+public class b {
+    int f;
+    int g;
+
+    public b(int f, int g) {
+        this.f = f;
+        this.g = g;
+    }
+    
+}
