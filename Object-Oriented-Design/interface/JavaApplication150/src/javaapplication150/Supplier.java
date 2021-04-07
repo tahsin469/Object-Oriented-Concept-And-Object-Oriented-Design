d676080015e220f7151cd586a8cfee908ba43cd2
Md.Tahsinul Islam <mughdaw825@gmail.com>
2021-04-08 00:03:36 +0600

    Update Supplier.java

--- Object-Oriented-Design/interface/JavaApplication150/src/javaapplication150/Supplier.java
+++ Object-Oriented-Design/interface/JavaApplication150/src/javaapplication150/Supplier.java
@@ -2,11 +2,12 @@
 package javaapplication150;
 
 
-public class Supplier {
+public class Supplier implements IBonus{
     int soid_item;
     void set_sold_item(int s){
         soid_item = s;
     }
+    @Override
     public void getBonus(){
         System.out.println("Bonus is "+soid_item*15);
     }
